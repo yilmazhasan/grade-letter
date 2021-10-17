f380d865a31ece082baf3e70bef728eb031411b7
yilmazhasan <yilmazhasan111@gmail.com>
2021-10-17 16:31:05 +0300

    Remove trivial precatuion

--- js/init.js
+++ js/init.js
@@ -6,13 +6,6 @@ app.refreshRangesTable = undefined;
 app.refreshChart = undefined;
 app.categoryFreq = {};
 
-window.onload = () => {
-    // To prevent download and run code locally, trivial precauiton
-    if(window.location.href.indexOf("hasan") < 0 && window.location.href.indexOf("grade-letter") < 0 && window.location.href.indexOf("localhost:3001") < 0) {
-        $("body")[0].textContent = ""
-    }
-};
-
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
 });
